95d417f88f5e9c5ce4a3133fe2501c7f91fba40c
noblecoder11 <86159847+noblecoder11@users.noreply.github.com>
2021-09-24 23:48:49 +0530

    Organized files

--- OOPCGLab/Submited/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-void copy(personalInfo &pobj, Student &sobj1, personalInfo &pobj1, int n)
-    // {
-    //     Student obj1(*this);
-    //     personalInfo sobj1(pobj);
-         
-    
-        
-    // }
